0406d0a30d6b9c7011eda0a0ed9b82ca9f057299
Milica Todorovic <todorovicmilica018@gmail.com>
2023-08-16 13:04:53 +0200

    ÄHomework update 15.05.2023

---
+++ src/main/java/d15_08_2023/Zadatak7.java
@@ -0,0 +1,31 @@
+package d15_08_2023;
+
+public class Zadatak7 {
+    public static void main(String[] args) {
+
+
+        System.out.println("*****************************************");
+        System.out.println("*                                       *");
+        System.out.println("*                Register               *");
+        System.out.println("*              _________________        *");
+        System.out.println("*  Email:      |               |        *");
+        System.out.println("*               ***************         *");
+        System.out.println("*                                       *");
+        System.out.println("*                                       *");
+        System.out.println("*              _________________        *");
+        System.out.println("*  Password:   |               |        *");
+        System.out.println("*               ***************         *");
+        System.out.println("*                                       *");
+        System.out.println("*    __                                 *");
+        System.out.println("*   |  |  I Agree                       *");
+        System.out.println("*                                       *");
+        System.out.println("*                                       *");
+        System.out.println("*                ________________       *");
+        System.out.println("*               |                |      *");
+        System.out.println("*               |    Register    |      *");
+        System.out.println("*               |________________|      *");
+        System.out.println("*                                       *");
+        System.out.println("*                                       *");
+        System.out.println("*****************************************");
+    }
+}
