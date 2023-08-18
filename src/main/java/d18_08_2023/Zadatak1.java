e62c5a10972058e5ac7b9883b562b26e49d8c7c4
Milica Todorovic <todorovicmilica018@gmail.com>
2023-08-19 01:54:47 +0200

    Update Zadatak1 comment

--- src/main/java/d18_08_2023/Zadatak1.java
+++ src/main/java/d18_08_2023/Zadatak1.java
@@ -3,14 +3,13 @@ package d18_08_2023;
 import java.util.Scanner;
 
 public class Zadatak1 {
-    //if(a<10) {
-    //            System.out.println("Broj je jednocifren");}
-    //        else if (a<100){
-    //            System.out.println("broj je dvocifren");}
-    //        else if (a>100){
-    //            System.out.println("Broj je trocifren.");}
-    //        else {
-    //            System.out.println("Broj je visecifren");}
+    //Napisati program koji za unetu vrednost x odredjuje i prikazuje vrednost funkcije y. Iskoristite if/else za racunanje informacije i tek nakon te logike odstampajte vrednost y na ekranu.Funkcija y je definisana kao:
+    //    			       (x,       x < 2
+    //y =  (2,      2 <= x < 10
+    //    			       (x - 1, x >= 10
+    //Objasnjenje formule: Ako je x manje od 2, y je jednako x
+    //			 Ako je x u opsegu od 2 do 10, y je jednako 2
+    //			 Ako je x vece od 10, y je x-1
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
